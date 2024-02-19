adcdaa016ecde57596bd850036f02e4c9cb48cbb
Guilherme Puida <guilherme@puida.xyz>
2024-02-19 17:29:57 -0300

    Create main emulator class

--- chip8.js
+++ chip8.js
@@ -1,1 +1,43 @@
-console.log("hello, world!")
+const SCREEN_WIDTH = 64;
+const SCREEN_HEIGHT = 32;
+
+const RAM_SIZE = 4096;
+const NUM_REGS = 16;
+const NUM_KEYS = 16;
+const STACK_SIZE = 16;
+
+const START_ADDRESS = 0x200;
+
+class Chip8 {
+  #pc;
+  #ireg;
+  #ram;
+  #vreg;
+  #screen;
+  #keys;
+  #stack;
+  #sp;
+  #dt;
+  #st;
+
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.#pc = 0;
+    this.#ireg = 0;
+
+    this.#ram = new Uint8Array(RAM_SIZE);
+    this.#vreg = new Uint8Array(NUM_REGS);
+
+    this.#screen = new Array(SCREEN_WIDTH * SCREEN_HEIGHT).fill(false);
+    this.#keys = new Array(NUM_KEYS).fill(false);
+
+    this.#stack = new Uint16Array(STACK_SIZE);
+    this.#sp = 0;
+
+    this.#dt = 0;
+    this.#st = 0;
+  }
+}
