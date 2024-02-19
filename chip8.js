504f225bc305a060b7a5ae241534811849311ccb
Guilherme Puida <guilherme@puida.xyz>
2024-02-19 18:39:13 -0300

    Implement 8XY1: VX |= VY

--- chip8.js
+++ chip8.js
@@ -190,6 +190,12 @@ class Chip8 {
       return;
     }
 
+    // 8XY1: VX |= VY
+    if (d1 === 8 && d4 === 1) {
+      this.#vreg[d2] |= this.#vreg[d3];
+      return;
+    }
+
     // 9XY0: SKIP VX != VY
     if (d1 === 9 && d4 === 0) {
       if (this.#vreg[d2] !== this.#vreg[d3]) {
